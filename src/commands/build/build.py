134e6db7bcadbf3d046ccb3df9af236494c4c209
lembn <lembn@outlook.com>
2022-09-21 16:56:45 +0100

    finished todo in build.py

--- src/commands/build/build.py
+++ src/commands/build/build.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from typing import Any, Callable, TypeVar
 import zipfile
 
 import click
@@ -7,31 +8,53 @@ import yaml
 
 from .. import utils
 from . import descriptors
+from ... import utils as badkit_utils, wrappers as badkit_wrappers
+
+T = TypeVar("T")
 
 SRC = "src"
 BLEND = "blend"
 ADDON = "addon.yaml"
 BUILD = "build"
 
+
+if False:  # Test switch
+    SRC = os.path.join("test", SRC)
+    ADDON = os.path.join("test", ADDON)
+    BUILD = os.path.join("test", BUILD)
+
 for descriptor in descriptors.DESCRIPTOR_CLASSES:
     yaml.SafeLoader.add_constructor(descriptor.yaml_tag, descriptor.from_yaml)
+
+
+def run_in_dir(dir: str, func: Callable[[], T]) -> T:
+    cwd = os.getcwd()
+    os.chdir(dir)
+    ret = func()
+    os.chdir(cwd)
+    return ret
+
+
+def copy_to_zipf(zipf: zipfile.ZipFile, path: str, prefix: str = "") -> None:
+    for root, _, files in os.walk(path):
+        for file in files:
+            zipf.write(
+                os.path.join(root, file),
+                arcname=os.path.join(prefix, root, file),
+            )
 
 
 @click.command()
 def build() -> None:
     """Build an addon bundle from a specified source directory."""
 
-    src_dir = os.path.abspath(SRC)
     addon_path = os.path.abspath(ADDON)
     if not os.path.exists(addon_path):
-        raise FileNotFoundError(f"Failed to find {addon_path}.")
+        raise FileNotFoundError(f"Failed to find {ADDON}.")
 
     addon: descriptors.Addon = None
-    with open(addon_path, "r") as addon_file:
-        cwd = os.getcwd()
-        os.chdir(src_dir)
-        addon = yaml.safe_load(addon_file)
-        os.chdir(cwd)
+    with open(ADDON, "r") as addon_file:
+        addon = run_in_dir(SRC, lambda: yaml.safe_load(addon_file))
 
     # Prepare names/paths
     build_dir = os.path.abspath(BUILD)
@@ -52,15 +75,19 @@ def build() -> None:
             ),
             arcname="__init__.py",
         )
-        for root, _, files in os.walk(os.path.join(src_dir, BLEND)):
-            for file in files:
-                bundle.write(
-                    os.path.join(src_dir, BLEND, root, file),
-                    arcname=os.path.join(root, file),
-                )
-        # TODO: check that bundle is correct
-        # TODO: copy utils into bundle
-        # TODO: copy wrappers into bundle
+
+        run_in_dir(SRC, lambda: copy_to_zipf(bundle, BLEND))
+        copy_badkit_module_to_bundle: Callable[
+            [str], None
+        ] = lambda filename: run_in_dir(
+            os.path.dirname(os.path.dirname(filename)),
+            lambda: copy_to_zipf(
+                bundle, os.path.basename(os.path.dirname(filename)), prefix="badkit"
+            ),
+        )
+        copy_badkit_module_to_bundle(badkit_utils.__file__)
+        copy_badkit_module_to_bundle(badkit_wrappers.__file__)
+
         bundle.writestr("classes.pkl", pickle.dumps(addon.get_classes()))
         bundle.writestr("blend.pkl", pickle.dumps(addon.blend))
 
